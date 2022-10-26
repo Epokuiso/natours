02352813c1a555472e3dd4d14fb7c81b72d7ac63
Epokuiso <epokuiso@gmail.com>
2022-10-26 09:57:52 +0100

    handling get requests

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require ('express');
+const fileSystem = require('fs');
 
 const app = express ();
-
-
-const PORT = 3000;
-
+/*
 app.get ('/', (request, response) =>
 {
     
@@ -13,7 +11,24 @@ app.get ('/', (request, response) =>
         .json ({message: 'Hello from the server side.', app: 'Natours'});
 }); 
 
+*/
 
+const tours = JSON.parse(fileSystem.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+
+app.get ('/api/tours', (request, response) => 
+{
+    response
+        .status (200)
+        .json ({
+            status: "success",
+            results: tours.length,
+            data: tours 
+        });
+});
+
+
+const PORT = 3000;
 app.listen(PORT, () => 
 {
     console.log (`App running on PORT: ${PORT}.`);
