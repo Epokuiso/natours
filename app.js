698b0fd7c759fd8b5880456bed01412afe44309b
Epokuiso <epokuiso@gmail.com>
2022-10-28 16:26:38 +0100

    serving static files

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express ();
 -----------------------------------------------------------*/
 app.use (morgan ('dev'));
 app.use (express.json());
+app.use (express.static (`${__dirname}/public`));
 
 app.use ((request, response, next) =>
 {
