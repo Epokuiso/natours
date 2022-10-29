130c16ec30d9b21159473aadf6cd3c01af51d651
Epokuiso <epokuiso@gmail.com>
2022-10-29 14:09:51 +0100

    environment variables

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const app = express ();
 /*----------------------------------------------------------
     Middlewares
 -----------------------------------------------------------*/
-app.use (morgan ('dev'));
+if (process.env.NODE_ENV === "development")
+{
+    app.use (morgan ('dev'));
+}
 app.use (express.json());
 app.use (express.static (`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const dotenv = require ('dotenv');
+dotenv.config ({ path: './config.env' });
+
 const app = require ('./app');
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => 
 {
     console.log (`App running on PORT: ${PORT}.`);
