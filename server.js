dd821c25ebd8ce11128c053aa665ff2084389166
Epokuiso <epokuiso@gmail.com>
2022-11-01 03:43:11 +0100

    creating a simple tour model

--- server.js
+++ server.js
@@ -15,6 +15,25 @@ mongoose.connect (DATABASE_CONNECTION_STRING, {
     useFindAndModify: false
 }).then (() => console.log ('Database connection established.'));
 
+const tourSchema = new mongoose.Schema ({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name.'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price.']
+    }
+
+});
+
+const TourModel = mongoose.model ('Tour', tourSchema);
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => 
 {
